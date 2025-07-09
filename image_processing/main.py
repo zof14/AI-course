886ddc0a7d10aa0459bdc10bb467fd955be1f551
Zofia S <zosiasadel11@gmail.com>
2025-07-09 21:48:48 +0200

    image caption

---
+++ image_processing/main.py
@@ -0,0 +1,12 @@
+
+from transformers import BlipProcessor, BlipForConditionalGeneration
+from PIL import Image
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+image = Image.open("dog.jpeg")
+inputs = processor(image, return_tensors="pt")
+outputs = model.generate(**inputs)
+caption = processor.decode(outputs[0],skip_special_tokens=True)
+ 
+print("Generated Caption:", caption)
